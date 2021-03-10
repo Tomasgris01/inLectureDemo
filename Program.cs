5bda58adc07a1e2308b84c9bafdc0f9e3a7a5444
Tomasgris01 <tomasgris14@gmail.com>
2021-03-10 18:34:48 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace inLectureDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle box; // declare variable of type rectangle
+            box = new Rectangle(); // recreating an object of type rectangle
+            box.length = 10;
+            box.width = 30;
+            int area = box.calArea();
+            Console.WriteLine("The area of box 1 is {0}",area);
+            Console.WriteLine(box.ToString());
+
+
+
+            Rectangle box2;
+            box2 = new Rectangle();
+
+            box2.length = 6;
+            box2.width = 8;
+             area = box2.calArea();
+            Console.WriteLine("The area of box 2 is {0}", area);
+
+
+            Rectangle box3;
+            box3 = new Rectangle(5,10);
+
+            
+            area = box3.calArea();
+            Console.WriteLine("The area of box 2 is {0}", area);
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inLectureDemo
+{
+    class Rectangle
+    {
+        //attributes of the class
+        public int length;
+        public int width;
+
+        //Methods
+        //constructor method - must call this before we can use the class
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(int l, int w)
+        {
+            length = l;
+            width = w;
+        }
+
+        public int calArea()
+        {
+            return length * width;
+        }
+       
+        
+        public virtual string ToString () // special method that tells us about the object
+         {
+
+            return "I am a rectangle with lenght  " + length;
+
+         }
+
+    }
+}
